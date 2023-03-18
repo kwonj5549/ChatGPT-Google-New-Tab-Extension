67b5fe719748c5dfe65639b3789378d00fe5e9dd
kwill5800 <90160910+kwill5800@users.noreply.github.com>
2023-03-17 17:02:12 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -6,3 +6,19 @@ chrome.runtime.onInstalled.addListener(function (details) {
         chrome.tabs.create({ url: "https://chat.openai.com/auth/login" });
     }
 });
+
+chrome.webRequest.onHeadersReceived.addListener(
+
+  function (details) {
+    for (var j = 0; j < details.responseHeaders.length; ++j) {
+      if (details.responseHeaders[j].name.toLowerCase() == 'x-frame-options') {
+        details.responseHeaders.splice(j, 1);
+        
+        return {
+          responseHeaders: details.responseHeaders 
+        };
+      }
+    }
+  }, {
+    urls: ["<all_urls>"]
+  }, ["blocking", "responseHeaders"]);
